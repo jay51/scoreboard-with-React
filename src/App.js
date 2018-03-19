8563068eed0059adcf5c3fc26556a2cf53c51774
jay <nenojaafar3@gmail.com>
2018-03-18 23:59:34 -0500

    cleaned some of the code

--- src/App.js
+++ src/App.js
@@ -6,25 +6,7 @@ class App extends Component {
 
 	constructor(props) {
 		super(props);
-	  	this.state = {
-			value: '',
-			score:0
-	};
-
-	  	this.handleChange = this.handleChange.bind(this);
-	  	this.handleSubmit = this.handleSubmit.bind(this);
-	}
-
-	handleChange(event) {
-	  this.setState({value: event.target.value});
-	  console.log(event.target.value);
-	}
-
-	handleSubmit(event) {
-	  alert('A name was submitted: ' + this.state.value);
-	  event.preventDefault();
-
-	}
+}
 
 
   render() {
@@ -38,10 +20,6 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
 	  	</p>
-
-		<input type="text" onChange={this.handleChange} placeholder="player name"></input>
-		<input type="submit" onClick={this.handleSubmit} placeholder="Add"></input>
-
 
 		<Scoreboard/>
 
